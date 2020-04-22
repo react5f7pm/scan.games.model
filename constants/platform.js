f72100e13ff5273c25e456a5d56c19a7d8711f9a
frebern <frebern@naver.com>
2020-04-22 20:04:46 +0900

    [YG] platform 상수 추가 (#1)
    * [YG] Submodule 테스트, platform 상수 추가
    * [YG] fix: Platform 모델 피드백 반영

---
+++ constants/platform.js
@@ -0,0 +1,10 @@
+const PLATFORM = {
+  STEAM: {
+    id: 'STEAM',
+    name: 'Steam',
+    homePage: 'https://store.steampowered.com/',
+    detailUrl: 'https://store.steampowered.com/app/',
+  },
+}
+
+export default PLATFORM
