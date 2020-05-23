15d5a59da5defb389fd439fc3cbca3a1dd15f127
frebern <frebern@naver.com>
2020-05-23 17:36:39 +0900

    [YG] Platform 모델에 플랫폼 추가

--- constants/platform.js
+++ constants/platform.js
@@ -5,6 +5,24 @@ const PLATFORM = {
     homePage: 'https://store.steampowered.com/',
     detailUrl: 'https://store.steampowered.com/app/',
   },
+  NINTENDO: {
+    id: 'NINTENDO',
+    name: 'Nintendo',
+    homePage: '',
+    detailUrl: '',
+  },
+  PLAYSTATION: {
+    id: 'PLAYSTATION',
+    name: 'PlayStation',
+    homePage: '',
+    detailUrl: '',
+  },
+  ORIGIN: {
+    id: 'ORIGIN',
+    name: 'Origin',
+    homePage: '',
+    detailUrl: '',
+  },
 }
 
 export default PLATFORM
